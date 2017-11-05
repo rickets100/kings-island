50a38042fee20d27924bad56f7e450ece2d3a27a
Kay Krewson <kay.k@comcast.net>
2017-11-04 18:35:52 -0700

    fixed the initial value for the image label

--- rides.php
+++ rides.php
@@ -13,13 +13,12 @@
 <script>
 	function onMouseOver(newImage, newText){
 		let hoverFun = document.getElementsByClassName("hover-fun")[0];
-		console.log("hoverFun",hoverFun)
-		let preview = hoverFun.getElementsByClassName('preview')[0];
-		let heyText = hoverFun.getElementsByClassName('lower')[0];
+		let preview = hoverFun.getElementsByClassName("preview")[0];
+		let prevLabel = hoverFun.getElementsByClassName("lower")[0];
 
 		preview.src = newImage;
-		heyText.innerHTML = newText;
-
+		// heyText.innerHTML = newText;
+		prevLabel.innerHTML = newText;
 	}
 </script>
 <table style="width:100%">
@@ -30,10 +29,7 @@
 					<?php foreach ($rides as $ride => $item) { ?>
 						<li>
 							<a href="ride.php?item=<?php echo $ride; ?>" onmouseover="
-onMouseOver('./assets/img/<?= $item["image"]; ?>.png', '<?= $item["name"]?>');
-
-								"><?php echo $item["name"]; ?></a>
-
+onMouseOver('./assets/img/<?= $item["image"]; ?>.png', '<?= $item["name"]?> &#8680;');"><?php echo $item["name"]; ?></a>
 						</li>
 
 					<?php } ?>
@@ -42,13 +38,11 @@ onMouseOver('./assets/img/<?= $item["image"]; ?>.png', '<?= $item["name"]?>');
 		</td>
     <td class="two-thirds">
 			<div class="hover-fun">
-				<a id="linkorama" href=""><img name="preview" class="preview" src="./assets/img/rc-backlot-300x180.png"/><div name="preview-label" class="lower"><?php echo $item["name"];?> &#8680;</div></a>
+				<a id="linkorama" href=""><img name="preview" class="preview" src="./assets/img/rc-backlot-300x180.png"/><div name="preview-label" class="lower">Backlot Stunt Coaster &#8680;</div></a>
 			</div><!-- hover-fun -->
 		</td>
   </tr>
 </table>
-<div name="hey" class="boring">HEY</div>
-
 <hr>
 <?php include('includes/footer.php'); ?>
 <!--
